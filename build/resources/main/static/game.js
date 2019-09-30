3fb4ac20eb6cf81d1713646c014813b2abf65d65
Nicolas Giraldez <klaus.15@gmail.com>
2019-09-30 20:01:04 -0300

    Create conditional grid colors
    Grilla muestra posición de los barcos por parámetro "gp" (gameplayer)

--- build/resources/main/static/game.js
+++ build/resources/main/static/game.js
@@ -1,62 +1,72 @@
+$(function() {
 
+    const urlParams = new URLSearchParams(window.location.search);
 
-function paramObj(search) {
-    var obj = {};
-    var reg = /(?:[?&]([^?&#=]+)(?:=([^&#]*))?)(?:#.*)?/g;
+    let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    let locations = [];
 
-    search.replace(reg, function(match, param, val) {
-        obj[decodeURIComponent(param)] = val === undefined ? "" : decodeURIComponent(val);
-    });
+    function getHeadersHtml(headers) {
+        return "<tr><th></th>" + headers.map(function(header) {
+            return "<th>" + header + "</th>";
+        }).join("") + "</tr>";
+    }
 
-    return obj;
-}
+    function renderHeaders() {
+        var html = getHeadersHtml(numbers);
+        document.getElementById("grid-numbers").innerHTML = html;
+    }
 
-
-let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-let locations = ["A1", "B4", "C2"];
-
-function getHeadersHtml(headers) {
-    return "<tr><th></th>" + headers.map(function(header) {
-        return "<th>" + header + "</th>";
-    }).join("") + "</tr>";
-}
-
-function renderHeaders() {
-    var html = getHeadersHtml(numbers);
-    document.getElementById("grid-numbers").innerHTML = html;
-}
-
-function getColumnsHtml(i) {
-    let html = "";
-    for (let j = 0; j < numbers.length; j++) {
-        let cellContent = "";
-        for (let k = 0; k < locations.length; k++) {
-            if (locations[k] == letters[i] + numbers[j]) {
-                cellContent = "si";
+    function getColumnsHtml(i) {
+        let html = "";
+        for (let j = 0; j < numbers.length; j++) {
+            let cellContent = "";
+            for (let k = 0; k < locations.length; k++) {
+                if (locations[k] == letters[i] + numbers[j]) {
+                    cellContent = "si";
+                }
             }
+            html = html + "<td>" + cellContent + "</td>";
         }
-        html = html + "<td>" + cellContent + "</td>";
+        return html;
     }
-    return html;
-}
 
-function getRowsHtml() {
-    let html = "";
-    for (let i = 0; i < letters.length; i ++) {
-        html = html + "<tr><th>" + letters[i] + "</th>" + getColumnsHtml(i) + "</tr>";
+    function getRowsHtml() {
+        let html = "";
+        for (let i = 0; i < letters.length; i ++) {
+            html = html + "<tr><th>" + letters[i] + "</th>" + getColumnsHtml(i) + "</tr>";
+        }
+        return html;
     }
-    return html;
-}
 
-function renderRows() {
-    var html = getRowsHtml();
-    document.getElementById("grid-rows").innerHTML = html;
-}
+    function renderRows() {
+        var html = getRowsHtml();
+        document.getElementById("grid-rows").innerHTML = html;
+    }
 
-function renderTable() {
-    renderHeaders();
-    renderRows();
-}
+    function renderTable() {
+        renderHeaders();
+        renderRows();
+    }
 
-renderTable();
+    function setLocations(data) {
+        locations = data.ships[0].locations
+        console.log(data.ships);
+        console.log(data.ships.map(function(ship) { return ship.locations}));
+    }
+
+    function loadData() {
+        $.get("/api/game_view/"+urlParams.get('gp'))
+            .done(function(data) {
+                //console.log(data);
+                setLocations(data);
+                renderTable();
+            })
+            .fail(function( jqXHR, textStatus ) {
+                alert( "Failed: " + textStatus );
+            });
+    }
+
+    loadData();
+
+});
